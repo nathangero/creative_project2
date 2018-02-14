f2a8a1d3c0bbfeb2779a71253f419e72e1a1430e
Nathan Geronimo <nathangero@yahoo.com>
2018-02-14 11:23:59 -0700

    added more css; pressing Enter now starts the search

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
 $(document).ready(function() { // Don't run until the webpage is fully loaded
 
-    $("#start").click(function(e) {
+    var placeholder = ['e.g. chicken,garlic,tomato','e.g. beef,carrots','e.g. pork,broccoli,onions','e.g. string beans,carrots,peas','e.g. flour,sugar,chocolate'];
+    var rand = Math.floor(Math.random() * (placeholder.length - 0)); //The maximum is inclusive and the minimum is inclusive 
+    // Sets the placeholder text to one of the strings typed out above
+    $("#ingredients").attr('placeholder',placeholder[rand]);
+
+    // Runs when the search button is pressed
+    $("#ingredients").keyup(function(event) { // This is when the enter key is hit
+        if (event.keyCode === 13) {
+            $("#search").click(); // Runs the onclick function for the Search button
+        }
+    });
+    $("#search").click(function(e) {
         e.preventDefault();
         var ingredients = $("#ingredients").val().split(',');
         console.log(ingredients);
@@ -16,6 +27,7 @@ $(document).ready(function() { // Don't run until the webpage is fully loaded
         var myurl = "http://www.recipepuppy.com/api/?" + query;
         console.log(myurl);
 
+        // Runs when connecting to the api
         $.ajax({
             url: myurl,
             dataType: "jsonp", // jsonp goes around the CORS issue
@@ -28,7 +40,10 @@ $(document).ready(function() { // Don't run until the webpage is fully loaded
                     results += '<li>\n<a href=' + json.results[i].href + ' target="_blank">' + json.results[i].title + '</a>\n<br><br>';
                 }
                 $("#recipeResults").html(results);
-                $("#relativeFooter").css('position', 'relative'); // Makes the footer become relative after successful search
+
+                if (json.results.length != 0) {
+                    $("#relativeFooter").css('position', 'relative'); // Makes the footer become relative after successful search
+                }
             }
         });
     });
