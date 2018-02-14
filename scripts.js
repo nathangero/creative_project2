0ec0b180b23783c087aff2d6086131ea9c5a5737
Nathan Geronimo <nathangero@yahoo.com>
2018-02-13 22:49:56 -0700

    added some css code

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() { // Don't run until the webpage is fully loaded
 
     $("#start").click(function(e) {
         e.preventDefault();
-        var ingredients = $("#ingredients").val().split(' ');
+        var ingredients = $("#ingredients").val().split(',');
         console.log(ingredients);
 
         var query = 'i=';
@@ -14,7 +14,7 @@ $(document).ready(function() { // Don't run until the webpage is fully loaded
         }
 
         var myurl = "http://www.recipepuppy.com/api/?" + query;
-        // console.log(myurl);
+        console.log(myurl);
 
         $.ajax({
             url: myurl,
@@ -22,7 +22,13 @@ $(document).ready(function() { // Don't run until the webpage is fully loaded
             jsonpCallback: "logResults", // Prevents the '?callback' string from being appended to the url
             success: function(json) { 
                 console.log(json) 
-                
+                var results = '';
+                for (var i = 0; i < json.results.length; i++) {
+                    results += '<img src="' + json.results[i].thumbnail + '">';
+                    results += '<li>\n<a href=' + json.results[i].href + ' target="_blank">' + json.results[i].title + '</a>\n<br><br>';
+                }
+                $("#recipeResults").html(results);
+                $("#relativeFooter").css('position', 'relative'); // Makes the footer become relative after successful search
             }
         });
     });
